2b95cc3b055fdca1c5072d7685ffbb8b5c060db4
Pedro Bento <pbentoit@gmail.com>
2025-03-06 19:45:42 +0000

    added missing if statements to ft_execute_builtin

--- src/execute_builtin.c
+++ src/execute_builtin.c
@@ -6,7 +6,7 @@
 /*   By: pda-silv <pda-silv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 14:41:32 by pda-silv          #+#    #+#             */
-/*   Updated: 2025/03/06 19:37:00 by pda-silv         ###   ########.fr       */
+/*   Updated: 2025/03/06 19:44:57 by pda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,15 @@ void	ft_execute_builtin(t_data *data, char **cmd_args)
 	len = ft_strlen(cmd) + 1;
 	if (!ft_strncmp(OP_EXT, cmd, len))
 		kill(getppid(), SIGTERM);
+	if (!ft_strncmp(OP_PWD, cmd, len))
+		return ;
+	if (!ft_strncmp(OP_ECHO, cmd, len))
+		return ;
+	if (!ft_strncmp(OP_ENV, cmd, len))
+		return ;
+	if (!ft_strncmp(OP_EXP, cmd, len))
+		return ;
+	if (!ft_strncmp(OP_UNS, cmd, len))
+		return ;
 	(void) data;
 }
